1e38549e46d5343b61b2da3d9a66946411cedf5c
oglock <og7ock@gmail.com>
2021-02-14 13:30:48 +0200

    Первый коммит

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    return render(request, 'main/create.html')
